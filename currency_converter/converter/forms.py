0a738187bdce01220a6b9eaa44b18c124dcbd742
AlexDoykov <doykov70@gmail.com>
2019-06-15 17:56:18 +0300

    some more validation

--- currency_converter/converter/forms.py
+++ currency_converter/converter/forms.py
@@ -1,13 +1,10 @@
 from django import forms
 from .models import Currency
+from decimal import Decimal
+from django.core.validators import MinValueValidator
 
 
 class ExchangeForm(forms.Form):
-    value = forms.DecimalField()
+    value = forms.DecimalField(validators=[MinValueValidator(Decimal('0.00'))])
     currency = forms.ChoiceField(choices=[(c.id, c.name) for c in Currency.objects.all().order_by("name")])
     converted_value = forms.DecimalField(disabled=True, required=False)
-
-   # def __init__(self, *args, **kwargs):
-   #     super(ExchangeForm, self).__init__(*args, **kwargs)
-   #     choices = [(c.id, c.name) for c in Currency.objects.all().order_by("name")]
-   #     self.fields['currency'].choices = choices 
--- currency_converter/converter/models.py
+++ currency_converter/converter/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 
 class Currency(models.Model):
-    name = models.CharField(max_length=20)
-    iso_code = models.CharField(max_length=5)
+    name = models.CharField(max_length=20, unique=True)
+    iso_code = models.CharField(max_length=5, unique=True)
     exchange_rate = models.DecimalField(max_digits=5, decimal_places=3)
 
+    def __str__(self):
+        return self.name + " " + self.iso_code
 
 # class ExchangeRate(models.Model):
 #     from_currency = models.ForeignKey(
