429eb5df4b0b29084abbd165a1f9fdd6491d7d33
Alexander <doykov70@gmail.com>
2019-08-09 09:05:41 +0300

    refactor sync command for terminal

--- currency_converter/converter/management/commands/synchronise_with_bnb.py
+++ currency_converter/converter/management/commands/synchronise_with_bnb.py
@@ -1,10 +1,6 @@
 from django.core.management.base import BaseCommand
-from converter.models import Currency, ExchangeRate
-import requests
-from bs4 import BeautifulSoup
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-
         self.stdout.write(self.style.SUCCESS('Successfully downloaded file'))
