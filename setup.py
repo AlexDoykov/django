d0286693a7b5cd4f8c2c60cb7c2a3ad89a3d6392
AlexDoykov <doykov70@gmail.com>
2019-08-12 09:06:37 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="currency_converter",
+    version="2.0",
+    author_email="doykov70@gmail.com",
+    description="well a currency converter,
+    packages = ["currency_converter",]
+)
