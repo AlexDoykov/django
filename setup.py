8fbdd97490b38ae9f8358b9d9bf2d6aae74006d0
AlexDoykov <doykov70@gmail.com>
2019-08-12 11:07:15 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="currency_converter",
-    version="2.0",
-    author_email="doykov70@gmail.com",
-    description="well a currency converter",
-    packages = ["currency_converter/converter",]
-)
