512af0f46910104ff2de00a952ce7f9704659817
Alexander <doykov70@gmail.com>
2019-08-14 09:08:02 +0300

    additional settings for django cms

--- currency_converter/currency_converter/settings.py
+++ currency_converter/currency_converter/settings.py
@@ -37,11 +37,12 @@ SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
+    'djangocms_admin_style',
+    'django.contrib.messages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'converter.apps.ConverterConfig',
@@ -52,7 +53,18 @@ INSTALLED_APPS = [
     'treebeard',  # utilities for implementing a tree
     'menus',  # helper for model independent hierarchical website navigation
     'sekizai',  # for javascript and css management
-    'djangocms_admin_style',  # for the admin skin. You **must** add 'djangocms_admin_style' in the list **before** 'django.contrib.admin'.
+    'filer',
+    'easy_thumbnails',
+    'mptt',
+    'djangocms_text_ckeditor',
+    'djangocms_link',
+    'djangocms_file',
+    'djangocms_picture',
+    'djangocms_video',
+    'djangocms_googlemap',
+    'djangocms_snippet',
+    'djangocms_style',
+    'djangocms_column',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +76,14 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+     # Django CMS middlewares
+     'cms.middleware.utils.ApphookReloadMiddleware',
+     'cms.middleware.user.CurrentUserMiddleware',
+     'cms.middleware.page.CurrentPageMiddleware',
+     'cms.middleware.toolbar.ToolbarMiddleware',
+     'cms.middleware.language.LanguageCookieMiddleware',
 ]
 
 ROOT_URLCONF = 'currency_converter.urls'
@@ -87,6 +106,22 @@ TEMPLATES = [
         },
     },
 ]
+
+CMS_TEMPLATES = (
+    ## Customize this
+    ('fullwidth.html', 'Fullwidth'),
+    ('sidebar_left.html', 'Sidebar Left'),
+    ('sidebar_right.html', 'Sidebar Right'),
+)
+
+THUMBNAIL_HIGH_RESOLUTION = True
+
+THUMBNAIL_PROCESSORS = (
+    'easy_thumbnails.processors.colorspace',
+    'easy_thumbnails.processors.autocrop',
+    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'easy_thumbnails.processors.filters'
+)
 
 WSGI_APPLICATION = 'currency_converter.wsgi.application'
 
@@ -125,11 +160,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGES = (
-    ('en-us', _('English')),
+    ('en', _('English')),
     ('bg', _('Bulgarian')),
 )
 
-LANGUAGE_CODE = 'en-us' 'bg'
+LANGUAGE_CODE = 'en'
 
 
 TIME_ZONE = 'UTC'
@@ -144,7 +179,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
